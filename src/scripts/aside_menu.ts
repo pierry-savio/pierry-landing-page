1690a18ebf3ddcf6e1c4b685e87ada4d2dd9de8e
pierry-savio <pierrysavio.pi@gmail.com>
2026-08-29 11:02:56 -0300

    feat: closing the mobile header by clicking out of the header

--- src/scripts/aside_menu.ts
+++ src/scripts/aside_menu.ts
@@ -1,7 +1,13 @@
 const menu_icon = document.getElementById("menu_icon") as HTMLElement;
 const ul_mobile = document.getElementById("ul_mobile") as HTMLElement;
+const main = document.getElementsByTagName("main")[0];
 
-menu_icon.addEventListener("click", ()=>{
+menu_icon.addEventListener("click", () => {
     menu_icon.classList.toggle("opened");
     ul_mobile.classList.toggle("closed");
-})
+});
+
+main.addEventListener("click", () => {
+    menu_icon.classList.add("opened");
+    ul_mobile.classList.add("closed");
+});
