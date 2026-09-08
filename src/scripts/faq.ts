591f458acb8d4373f1c585372f6f6fa59a34686c
pierrysavio.pi@gmail.com <pierrysavio.pi@gmail.com>
2026-09-08 11:56:03 -0300

    feat: faq

---
+++ src/scripts/faq.ts
@@ -0,0 +1,7 @@
+const questions = document.getElementsByClassName("question");
+
+for (let i = 0; i < questions.length; i++){
+    questions[i].addEventListener("click", () =>{
+        questions[i].classList.toggle("opened");
+    });
+}
