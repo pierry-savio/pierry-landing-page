9744438ccfbbb881308f0d68c9cdd6e499ed066a
pierry <pierrysavio.pi@gmail.com>
2026-09-15 08:33:56 -0300

    fix: section steps badge desktop

--- src/scripts/scroll_smooth.ts
+++ src/scripts/scroll_smooth.ts
@@ -1,9 +1,12 @@
-import Lenis from 'lenis';
+import Lenis from 'lenis'
 
-const lenis = new Lenis();
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+})
 
 function raf(time: number) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+  lenis.raf(time)
+  requestAnimationFrame(raf)
 }
-requestAnimationFrame(raf);
+requestAnimationFrame(raf)
